ce89a3b39c931e376acd730b4a69c301ad336dce
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-11-16 11:34:56 +0530

    added logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,9 @@
+import { Navbar } from "../(marketing)/_components/navbar";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
+      <Navbar />
       <main className="pt-40 pb-20">{children}</main>
     </div>
   );
