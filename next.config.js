d6b54cda57365f366714f5b74f652dd5b7484c19
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-11-16 11:41:34 +0530

    added auth api

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  webpack: (config) => {
+    // nextjs does not play nice with knex, which @theniledev/server uses under the hood
+    config.externals.push({
+      knex: "commonjs knex",
+    });
+    return config;
+  },
+};
