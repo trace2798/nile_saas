da4472a9ce995b58597dd1e749854baa76c5b4dd
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-12-06 14:03:05 +0530

    added logic to add display status card

--- app/(platform)/dashboard/organization/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/dashboard/organization/board/[boardId]/_components/card-item.tsx
@@ -4,6 +4,7 @@ import { Draggable } from "@hello-pangea/dnd";
 
 import { useCardModal } from "@/hooks/use-card-modal";
 import { Card } from "@/types";
+import { Separator } from "@/components/ui/separator";
 
 interface CardItemProps {
   data: Card;
@@ -25,6 +26,14 @@ export const CardItem = ({ data, index }: CardItemProps) => {
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white dark:bg-zinc-800 rounded-md shadow-sm"
         >
           {data.title}
+          {data.status ? (
+            <>
+              <Separator className="bg-zinc-600 my-1" />
+              <h1 className="text-xs capitalize">{data.status}</h1>
+            </>
+          ) : (
+            <></>
+          )}
         </div>
       )}
     </Draggable>
