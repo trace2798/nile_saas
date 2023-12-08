7e2dced6d6d69858d20c481c0e1892c89d50b75e
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-12-08 16:47:59 +0530

    added color on status

--- app/(platform)/dashboard/organization/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/dashboard/organization/board/[boardId]/_components/card-item.tsx
@@ -36,7 +36,21 @@ export const CardItem = ({ data, index, userInfo }: CardItemProps) => {
           <div className="flex flex-row justify-between">
             {data.status ? (
               <>
-                <h1 className="text-xs capitalize">{data.status}</h1>
+                <h1
+                  className={`text-xs capitalize ${
+                    data.status === "done"
+                      ? "text-blue-300"
+                      : data.status === "in-progress"
+                      ? "text-green-300"
+                      : data.status === "to-do"
+                      ? "text-orange-300"
+                      : ""
+                  }`}
+                >
+                  {data.status}
+                </h1>
+
+                {/* <h1 className="text-xs capitalize">{data.status}</h1> */}
               </>
             ) : (
               <></>
