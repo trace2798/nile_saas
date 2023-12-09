ddac153d34d2d32e5f1ad9ec8a050acc1d112b13
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-12-09 23:27:25 +0530

    removed badge from home page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -14,10 +14,10 @@ const MarketingPage = () => {
   return (
     <div className="flex items-center justify-center flex-col">
       <div className={cn("flex items-center justify-center flex-col")}>
-        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+        {/* <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
           Manage Task Efficiently
-        </div>
+        </div> */}
         <h1 className="text-3xl md:text-6xl text-center mb-6 font-switzerBold">
           Collab helps team move
         </h1>
